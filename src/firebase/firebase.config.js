6463f5413b17b562e70947ab43f698995bbca3e6
Md Galib Hossain <mdgalib23@gmail.com>
2022-11-08 02:14:47 +0600

    Project setup done

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDQZMqsKiCPAfGgePgF184dP_x4AgzQ3G8",
+  authDomain: "photography-service-3d7d2.firebaseapp.com",
+  projectId: "photography-service-3d7d2",
+  storageBucket: "photography-service-3d7d2.appspot.com",
+  messagingSenderId: "609464960671",
+  appId: "1:609464960671:web:89e5b02463ba5249cb84aa",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
